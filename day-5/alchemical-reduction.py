5eaccb409a837f26d95eca06da4e55c7ac2f1401
Jonathan Brumby <jonathan.brumby@excelian.com>
2018-12-09 12:55:34 +0000

    Day 5 - Part B - oops, forgot to commit this

--- day-5/alchemical-reduction.py
+++ day-5/alchemical-reduction.py
@@ -1,8 +1,8 @@
-def polymer_react(file_name):
-    f = open(file_name).read().replace('\n', '')
-    print(len(f))
+import re
+
+def react(polymer):
     reaction = []
-    for x in f:
+    for x in polymer:
         if len(reaction) == 0:
             reaction.append(x)
         else:
@@ -13,4 +13,18 @@ def polymer_react(file_name):
                 reaction.append(x)
     return len(reaction)
 
-print(str(polymer_react("input.txt")))
+def polymer_react(file_name):
+    f = open(file_name).read().replace('\n', '')
+    return react(f)
+
+def poly_remove_react(file_name):
+    f = open(file_name).read().replace('\n', '')
+    uniq = []
+    for x in set(f.lower()):
+        src_str  = re.compile(x, re.IGNORECASE)
+        str_replaced  = src_str.sub("",f)
+        uniq.append(react(str_replaced))
+    return min(uniq)
+
+print("Day 5 - Part A: " + str(polymer_react("input.txt")))
+print("Day 5 - Part B: " + str(poly_remove_react("input.txt")))
